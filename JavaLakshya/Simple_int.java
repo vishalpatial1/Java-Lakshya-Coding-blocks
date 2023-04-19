e3e6a2df0b75fc5f1d8f74ecc9653ee2d58fa771
vishalpatial1 <111500428+vishalpatial1@users.noreply.github.com>
2023-04-19 11:40:32 +0530

    Delete JavaLakshya directory

--- JavaLakshya/Simple_int.java
+++
@@ -1,9 +0,0 @@
-public class Simple_int {
-    public static void main(String[] args) {
-        int A = 1000;
-        int R = 10;
-        int T = 10;
-        int ans = A*R*T/100;
-        System.out.println(ans);
-    }
-}
--- JavaLakshya/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-Student s = new Student();
-        System.out.println(s.marks);
-    }
-}
-class Student {
-    public int marks;
-}
